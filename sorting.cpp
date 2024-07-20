6fc8742c8d7ed80041f470485f1b13b7d240d7cf
Arun silawat <arunsilawat722362@gmail.com>
2024-07-20 21:49:37 +0530

    Bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -87,11 +87,79 @@ using namespace std;
 //	print(ar,5);
 //}
 //-----------------------------------------------------------------
-void bubble(int a[],int n){
+//void bubble(int a[],int n){
+//	int temp,flag;
+//	for(int i=0;i<n-1;i++){
+//		flag=0;
+//		for(int j=0;j<n-i-1;j++){
+//			if(a[j]>a[j+1]){
+//				temp=a[j];
+//				a[j]=a[j+1];
+//				a[j+1]=temp;
+//				flag=1;
+//			}
+//		}
+//		if(flag==0)
+//		 break;
+//	}
+//}
+//void insertion(int a[],int n){
+//	int key,j;
+//	for(int i=1;i<n;i++){
+//		key=a[i];
+//		j=i-1;
+//		while(j>=0 && a[j]>key){
+//			a[j+1]=a[j];
+//			j--;
+//		}
+//		j++;
+//		a[j]=key;
+//	}
+//}
+//void selection(int a[],int n){
+//	int temp,index;
+//	for(int i=0;i<n-1;i++){
+//		index=i;
+//		for(int j=i+1;j<n;j++){
+//			if(a[index]>a[j]){
+//				index=j;
+//			}
+//		}
+//		if(i!=index){
+//			temp=a[i];
+//			a[i]=a[index];
+//			a[index]=temp;
+//		}
+//	}
+//}
+//void print(int A[],int n){
+//	for(int i=0;i<n;i++){
+//		cout<<A[i]<<" ";
+//	}
+//}
+//int main(){
+//	int ar[5]={17,14,3,21,5};
+//	print(ar,5);
+//	cout<<endl;
+//	cout<<"Bubble sort"<<endl;
+//	bubble(ar,5);
+//	print(ar,5);
+//	cout<<endl;
+//	cout<<"Insertion sort"<<endl;
+//	insertion(ar,5);
+//	print(ar,5);
+//	cout<<endl;
+//	cout<<"selection sort"<<endl;
+//	insertion(ar,5);
+//	print(ar,5);
+//}
+
+//---------------------------------------
+void bubble_sort(int a[],int n){
 	int temp,flag;
 	for(int i=0;i<n-1;i++){
 		flag=0;
-		for(int j=0;j<n-i-1;j++){
+		for(int j=0;n-i-1;j++){
 			if(a[j]>a[j+1]){
 				temp=a[j];
 				a[j]=a[j+1];
@@ -99,57 +167,20 @@ void bubble(int a[],int n){
 				flag=1;
 			}
 		}
-		if(flag==0)
-		 break;
-	}
-}
-void insertion(int a[],int n){
-	int key,j;
-	for(int i=1;i<n;i++){
-		key=a[i];
-		j=i-1;
-		while(j>=0 && a[j]>key){
-			a[j+1]=a[j];
-			j--;
-		}
-		j++;
-		a[j]=key;
-	}
-}
-void selection(int a[],int n){
-	int temp,index;
-	for(int i=0;i<n-1;i++){
-		index=i;
-		for(int j=i+1;j<n;j++){
-			if(a[index]>a[j]){
-				index=j;
-			}
-		}
-		if(i!=index){
-			temp=a[i];
-			a[i]=a[index];
-			a[index]=temp;
+		if(flag==0){
+			break;
 		}
 	}
 }
-void print(int A[],int n){
+void print(int a[],int n){
 	for(int i=0;i<n;i++){
-		cout<<A[i]<<" ";
+		cout<<a[i]<<" ";
 	}
 }
 int main(){
-	int ar[5]={17,14,3,21,5};
-	print(ar,5);
+	int ar[6]={17,22,3,55,87,23};
+	print(ar,6);
 	cout<<endl;
-	cout<<"Bubble sort"<<endl;
-	bubble(ar,5);
-	print(ar,5);
-	cout<<endl;
-	cout<<"Insertion sort"<<endl;
-	insertion(ar,5);
-	print(ar,5);
-	cout<<endl;
-	cout<<"selection sort"<<endl;
-	insertion(ar,5);
-	print(ar,5);
+	bubble_sort(ar,6);
+	print(ar,6);
 }
