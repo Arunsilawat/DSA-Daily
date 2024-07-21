bc42ace8dda60220a375721c6d93849f6c34f763
Arun silawat <arunsilawat722362@gmail.com>
2024-07-21 22:58:00 +0530

    merge sort

--- sorting.cpp
+++ sorting.cpp
@@ -201,7 +201,39 @@ void selection(int a[],int n){
 		}
 	}
 }
- 
+ void merge(int a[],int low,int mid,int high){
+	int i=low,j=mid+1,k=0,size=high-low+1,temp[size];
+	while(i<mid && j<high){
+		if(a[i]<=a[j]){
+			temp[k]=a[i];
+			i++,k++;
+		}
+		else{
+			temp[k]=a[j];
+			j++,k++;
+		}
+	}
+	while(i<mid){
+		temp[k]=a[i];
+		i++,k++;
+	}
+	while(j<high){
+		temp[k]=a[j];
+		j++,k++;
+	}
+	for(int i=0;i<size;i++){
+		a[low]=temp[i];
+		low++;
+	}
+}
+void merge_sort(int a[],int low,int high){
+	if(low<high){
+		int mid=(high+low)/2;
+    	merge_sort(a,low,mid);
+    	merge_sort(a,mid+1,high);
+    	merge(a,low,mid,high);
+	}
+}
 void print(int a[],int n){
 	for(int i=0;i<n;i++){
 		cout<<a[i]<<" ";
@@ -218,5 +250,8 @@ int main(){
 	print(ar,6);
 	cout<<endl;
 	selection(ar,6);
+	print(ar,6);
+	cout<<endl;
+	merge_sort(ar,0,5);
 	print(ar,6);
 }
