e73cdda2fb4277c045563725fcaaafdfb27de981
Arun silawat <arunsilawat722362@gmail.com>
2024-07-21 14:44:15 +0530

    Bubble sort Insertion and Selection sort

--- sorting.cpp
+++ sorting.cpp
@@ -155,20 +155,49 @@ using namespace std;
 //}
 
 //---------------------------------------
-void bubble_sort(int a[],int n){
+void Bubble(int A[],int n){
 	int temp,flag;
-	for(int i=0;i<n-1;i++){
+	for (int i=0;i<n-1;i++){
 		flag=0;
-		for(int j=0;n-i-1;j++){
-			if(a[j]>a[j+1]){
-				temp=a[j];
-				a[j]=a[j+1];
-				a[j+1]=temp;
+		for(int j=0;j<n-i-1;j++){
+			if(A[j]>A[j+1]){
+				temp=A[j];
+				A[j]=A[j+1];
+				A[j+1]=temp;
 				flag=1;
 			}
 		}
 		if(flag==0){
 			break;
+		}
+	}
+}
+void insertion(int A[],int n){
+	int key,j;
+	for(int i=1;i<n;i++){
+		key=A[i];
+		j=i-1;
+		while(j>=0 && A[j]>key){
+			A[j+1]=A[j];
+			j--;
+		}
+		j++;
+		A[j]=key;
+	}
+}
+void selection(int a[],int n){
+	int temp,index;
+	for(int i=0;i<n-1;i++){
+		index=i;
+		for(int j=i+1;j<n;j++){
+			if(a[index]>a[j]){
+				index=j;
+			}
+		}
+		if(i!=index){
+			temp=a[i];
+			a[i]=a[index];
+			a[index]=temp;
 		}
 	}
 }
@@ -178,9 +207,15 @@ void print(int a[],int n){
 	}
 }
 int main(){
-	int ar[6]={17,22,3,55,87,23};
+	int ar[6]={90,9,23,5,43,12};
 	print(ar,6);
 	cout<<endl;
-	bubble_sort(ar,6);
+	Bubble(ar,6);
+	print(ar,6);
+	cout<<endl;
+	insertion(ar,6);
+	print(ar,6);
+	cout<<endl;
+	selection(ar,6);
 	print(ar,6);
 }
